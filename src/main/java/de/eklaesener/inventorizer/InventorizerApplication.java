dbe51319da1de453962a712ef5fd6bc5cabe820c
Elias (eklaesener) <elias.klaesener@uni-bielefeld.de>
2020-07-19 15:08:25 +0200

    Initial commit

---
+++ src/main/java/de/eklaesener/inventorizer/InventorizerApplication.java
@@ -0,0 +1,13 @@
+package de.eklaesener.inventorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eklaesener/inventorizer/InventorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eklaesener.inventorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
